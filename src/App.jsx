067db1fe1a2a58ea87d66e86394475d801823933
jaikrishnaverma <jaikrishnaverma>
2022-10-11 18:47:20 +0530

    dgfd

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="div1"></div>
+
+      <div className="div22 row flexSB">
+        <div className="col50 cll">
+<p className="p1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab rerum voluptas non similique. Labore laboriosam voluptate deserunt quam vero blanditiis!
+</p>
+<p className="p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus excepturi, totam natus eligendi id eos reprehenderit. Nisi, expedita.</p>
+        </div>
+        <div className="col50 box col flexAIC flexJCC">
+          <div className='col  form'>
+            <div className="row flexJCC">
+              <input type="text" /><input type="text" />
+            </div>
+            <div className="row flexJCC">
+              <input type="text" /><input type="text" />
+            </div>
+            <div className="row flexJCC">
+              <input type="text" /><input type="text" />
+            </div>
+           <div className="row "> <button>PREQUALIFY ME</button></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="div2"></div>
+      <div className="div3"></div>
+      <div className="div4"></div>
+      <div className="div5"></div>
+
+    </>
+  );
+}
+
+export default App;
